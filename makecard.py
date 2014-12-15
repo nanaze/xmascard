c5088c72c8a13c207a6470dee8e47d0ced333b89
Nathan Naze <nanaze@gmail.com>
2014-12-14 23:02:59 -0500

    Drop svgwriter in favor of just modifying the DOM.
    Parse the bullet files and append them directly.

--- makecard.py
+++ makecard.py
@@ -2,15 +2,48 @@
 
 import sys
 import svgwrite
+from xml.dom import minidom
+
+def _LoadSvg(path):
+  doc = minidom.parse(path)
+  doc_frag = minidom.DocumentFragment()
+  for node in doc.childNodes:
+    doc_frag.appendChild(node)
+  return doc_frag
 
 def main():
-  drawing = svgwrite.Drawing(size=('1000', '1400'))
 
-  img = svgwrite.image.Image('bullets/NYCS-bull-trans-1.svg',insert=(100, 100), size=(100,100))
+  doc = minidom.Document()
+
+  svg = doc.createElement('svg')
+  svg.setAttribute('xmlns', "http://www.w3.org/2000/svg")
+
+  doc.appendChild(svg)
+
+  # Append a bullet. translate should be a tuple.
+  def AppendBullet(bullet_name, translate=None):
+
+    g = doc.createElement('g')
+    svg.appendChild(g)
+
+    if translate:
+      g.setAttribute('transform', 'translate(%s, %s)' % translate)
+
+    path = 'bullets/NYCS-bull-trans-%s.svg' % bullet_name
+    bullet_svg = _LoadSvg(path)
+    g.appendChild(bullet_svg)
+
+  # Just for now, paint out all the bullets.
+  y = 0
+  for letter in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'J', 'L', 'M', 'N', 'Q', 'R', 'S', 'Z'] + range(1,8):
+    AppendBullet(letter, (0, y))
+    y += 50
   
-  drawing.add(img)
-  
-  sys.stdout.write(drawing.tostring())
+  xmlstr = doc.toprettyxml()
+
+  # drop the first line, the <?xml ...> bit.
+  for line in xmlstr.splitlines(True)[1:]:
+    sys.stdout.write(line)
   
 
 if __name__ == '__main__':
