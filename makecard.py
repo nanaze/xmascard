abff14b5804bf43bc2bffeac6418259580bdbae5
Nathan Naze <nanaze@gmail.com>
2014-12-14 19:53:17 -0500

    Include the first bullet svg

--- makecard.py
+++ makecard.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
+import sys
 import svgwrite
 
 def main():
-  print 'test'
+  drawing = svgwrite.Drawing(size=('1000', '1400'))
+
+  img = svgwrite.image.Image('bullets/NYCS-bull-trans-1.svg',insert=(100, 100), size=(100,100))
+  
+  drawing.add(img)
+  
+  sys.stdout.write(drawing.tostring())
+  
 
 if __name__ == '__main__':
   main()
